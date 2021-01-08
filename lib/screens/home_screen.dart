d9ad0ffa63b34a07498970adcbcbb9f3a042ccaa
Phúc Võ <you@example.com>
2021-01-08 11:49:53 +0700

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -49,69 +49,86 @@ class _HomeScreenState extends State<HomeScreen> {
       myProfileContent(context),
     ];
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: null,
-        title: Column(
-          children: [
-            SizedBox(
-              height: 40.0,
-              child: TextFormField(
-                controller: _searchController,
-                focusNode: searchFocusNode,
-                textAlignVertical: TextAlignVertical.center,
-                textAlign: TextAlign.left,
-                maxLines: 1,
-                decoration: InputDecoration(
-                  hintText: "Search",
-                  prefixIcon: Icon(Icons.search),
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 0, horizontal: 14.0),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: colorConstant.greyColor,
-                    ),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(22.0),
-                    ),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(22.0),
-                    ),
-                    borderSide: BorderSide(
-                      color: colorConstant.greyColor,
-                    ),
-                  ),
-                  hintStyle: TextStyle(
-                    color: colorConstant.greyColor,
-                    letterSpacing: -0.4,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-        actions: [
-          Padding(
-            padding: EdgeInsets.only(right: 16.0),
-            child: SizedBox(
-              width: 40.0,
-              height: 40.0,
-              child: CircleAvatar(
-                backgroundColor: colorConstant.greyColor,
-                radius: 20,
-                child: IconButton(
-                  padding: EdgeInsets.zero,
-                  icon: Icon(Icons.filter_list),
-                  color: Colors.white,
-                  onPressed: () {},
-                ),
-              ),
-            ),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: Theme(
+          data: ThemeData(
+            highlightColor: Colors.transparent,
+            splashColor: Colors.transparent,
           ),
-        ],
+          child: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            leading: null,
+            title: Column(
+              children: [
+                SizedBox(
+                  height: 40.0,
+                  child: TextFormField(
+                    controller: _searchController,
+                    focusNode: searchFocusNode,
+                    textAlignVertical: TextAlignVertical.center,
+                    textAlign: TextAlign.left,
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      prefixIcon: Icon(Icons.search),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 0, horizontal: 14.0),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: colorConstant.greyColor,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(22.0),
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: colorConstant.primaryColor,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(22.0),
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(22.0),
+                        ),
+                        borderSide: BorderSide(
+                          color: colorConstant.greyColor,
+                        ),
+                      ),
+                      hintStyle: TextStyle(
+                        color: colorConstant.greyColor,
+                        letterSpacing: -0.4,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            actions: [
+              Padding(
+                padding: EdgeInsets.only(right: 16.0),
+                child: SizedBox(
+                  width: 40.0,
+                  height: 40.0,
+                  child: CircleAvatar(
+                    backgroundColor: colorConstant.greyColor,
+                    radius: 20,
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: Icon(Icons.filter_list),
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
